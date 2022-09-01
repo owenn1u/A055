e3c30c8165ffc1d76c320236c90f0fa0e3ef95ba
owen1u <cheng.lin.lu.2003@gmail.com>
2022-09-01 11:31:45 +0800

    advanced version

--- A055.py
+++ A055.py
@@ -1,6 +1,6 @@
 data = []
 count = 0
-
+length = 0
 
 with open('reviews.txt', 'r') as f:
 	for line in f:
@@ -8,3 +8,9 @@ with open('reviews.txt', 'r') as f:
 		count += 1
 		if count % 1000 == 0:
 			print(len(data))
+
+sum_len = 0
+for d in data:
+	sum_len = sum_len + len(d)
+
+print('留言平均長度是', sum_len / len(data))
