29d7ab702b4d609061ed8278c1e9bf00cc5ca285
owen1u <cheng.lin.lu.2003@gmail.com>
2022-09-01 11:45:45 +0800

    A056 version

--- A055.py
+++ A055.py
@@ -6,11 +6,23 @@ with open('reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line)
 		count += 1
-		if count % 1000 == 0:
+		if count % 10000 == 0:
 			print(len(data))
 
 sum_len = 0
 for d in data:
 	sum_len = sum_len + len(d)
-
 print('留言平均長度是', sum_len / len(data))
+
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('共有', len(new), '筆資料小於100字')
+print(new[0])
+
+big = []
+for d in data:
+	if 'big' in d:
+		big.append(d)
+print('共有', len(big), '筆資料提到 big')
